56d411327c3bb056e80456f2535de14dbbb3c783
handou26 <251585741@qq.com>
2019-02-20 11:57:40 +0800

    添加权限过滤器

--- src/main/java/com/jd/ClouldGatewayApplication.java
+++ src/main/java/com/jd/ClouldGatewayApplication.java
@@ -5,9 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients     //开启Fegin
 public class ClouldGatewayApplication {
 	
 	  private static final Logger logger = LoggerFactory.getLogger(ClouldGatewayApplication.class);
@@ -17,9 +20,10 @@ public class ClouldGatewayApplication {
 //	      
 //		   return builder.routes()
 //	               .route(p -> p
-//	                       .path("/get")
-//	                       .filters(f -> f.addRequestHeader("Hello", "World"))
-//	                       .uri("http://httpbin.org:80"))
+//	                       .path("/orderapi/**")
+//	                       .filters(f -> f.filter(new ElapsedFilter()).stripPrefix(1).addResponseHeader("Hello", "World"))
+//	                       .uri("lb://ORDERSERVICE")
+//	                       .id("ORDERSERVICE"))
 //	               .build();
 //	   }
 	   
@@ -35,10 +39,15 @@ public class ClouldGatewayApplication {
 //	                  .build();
 //	    }
 //	    
+	   
+	   
+	   @Bean
+	   public TokenFilter tokenFilter(){
+		   
+		   return new TokenFilter();
+	   }
 	    
 		public static void main(String[] args) {
-			
-			logger.info("项目启动咯。。。。");
 			
 			SpringApplication.run(ClouldGatewayApplication.class, args);
 		}
